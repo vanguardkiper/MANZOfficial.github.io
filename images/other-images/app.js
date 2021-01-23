8d85a9179dd307a10e6cc285f25eadfd567855a8
Manuel Balbuena <32009029+vanguardkiper@users.noreply.github.com>
2021-01-23 10:29:10 +0800

    Added new Files

---
+++ images/other-images/app.js
@@ -0,0 +1,156 @@
+
+
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".itemBox");
+const original = document.querySelector(".full-img");
+
+previews.forEach(preview =>{
+    preview.addEventListener('click', () => {
+        modal.classList.add("open");
+        original.classList.add("open");
+        console.log( preview.getAttribute("data-original"));
+        const originalSrc = preview.getAttribute("data-original");
+        original.src=`${originalSrc}`;
+    });
+});
+
+modal.addEventListener('click',(e) =>{
+    if(e.target.classList.contains('modal')){
+        modal.classList.remove("open");
+    }
+});  
+
+
+
+
+
+
+
+
+
+
+
+const menu = document.querySelector('.logo-container');
+const menu_bar = document.querySelector('#menu-container');
+const logo_container = document.querySelector('.logo-container');
+const menu_content = document.querySelector('.menu-list');
+const menu_container = document.querySelector('.list-container');
+const menu_container2 = document.querySelector('.list-container2');
+const menu_container3 = document.querySelector('.list-container3');
+const menu_container4 = document.querySelector('.list-container4');
+const menu_container5 = document.querySelector('.list-container5');
+
+
+const skill1 = document.querySelector('.skill:nth-child(1)');
+const skill2 = document.querySelector('.skill:nth-child(2)');
+const skill3 = document.querySelector('.skill:nth-child(3)');
+const skill4 = document.querySelector('.skill:nth-child(4)');
+const skill5 = document.querySelector('.skill:nth-child(5)');
+const skill6 = document.querySelector('.skill:nth-child(6)');
+const h_skill = document.querySelector('.skill-type');
+const p_skill = document.querySelector('.skill-text');
+
+
+skill1.addEventListener('click',function(){
+
+    h_skill.innerHTML = "HTML";
+    p_skill.innerHTML = "Able to use HTML or Hypertext Markup Language to provide content and structure to my website. HTML refers to the tags which give information to the browser on how the page should be rendered.";
+});
+skill2.addEventListener('click',function(){
+
+    h_skill.innerHTML = "JAVA";
+    p_skill.innerHTML = "Capable of using JAVA language in creating different types of programs of various usage. It is a popular object-oriented programming language used in developing desktop and mobile applications.";
+});
+skill3.addEventListener('click',function(){
+
+    h_skill.innerHTML = "CSS";
+    p_skill.innerHTML = "Capable of using CSS or Cascading Style Sheets to incorporate design with the content provided by HTML. CSS allows the Web pages to be presented in multiple ways concerning layout, fonts, colors, etc.";
+});
+skill4.addEventListener('click',function(){
+
+    h_skill.innerHTML = "FIGMA";
+    p_skill.innerHTML = "Able to use FIGMA in designing websites and producing vector artworks. FIGMA refers to a vector graphics editor and a prototyping tool that mainly focuses on web designing. It has multiple features that can further help in web designing such as the plugins and wireframing features.";
+});
+skill5.addEventListener('click',function(){
+
+    h_skill.innerHTML = "JAVASCRIPT";
+    p_skill.innerHTML = "Capable of improving the user experience and the complexity of the design through Javascript. It provides interactive elements on the web pages which is used to interact with the user.";
+});
+skill6.addEventListener('click',function(){
+
+    h_skill.innerHTML = "WORDPRESS";
+    p_skill.innerHTML = "Able to use WordPress to develop a beautiful website. WordPress is a simple platform that provides easy website management and development.";
+});
+
+
+
+var checker = 0;
+const menu_display = document.querySelector('#menu-content');
+menu.addEventListener('click',function(){
+    if(checker == 0){
+        menu_bar.style.width="100%";
+        menu_bar.style.height="100%";
+        checker += 1;
+        console.log(checker);
+        
+        logo_container.style.background="white";
+        menu_container.style.opacity = "1";
+        menu_container.style.transform = "translateY(-20px)"; 
+        menu_container2.style.opacity = "1";
+        menu_container2.style.transform = "translateY(-20px)"; 
+        menu_container3.style.opacity = "1";
+        menu_container3.style.transform = "translateY(-20px)"; 
+        menu_container4.style.opacity = "1";
+        menu_container4.style.transform = "translateY(-20px)"; 
+        menu_container5.style.opacity = "1";
+        menu_container5.style.transform = "translateY(-20px)"; 
+        menu_container.style.cursor ="pointer";
+        menu_container2.style.cursor ="pointer";
+        menu_container3.style.cursor ="pointer";
+        menu_container4.style.cursor ="pointer";
+        menu_container5.style.cursor ="pointer";
+        menu_container.style.pointerEvents ="auto";
+        menu_container2.style.pointerEvents ="auto";
+        menu_container3.style.pointerEvents ="auto";
+        menu_container4.style.pointerEvents ="auto";
+        menu_container5.style.pointerEvents ="auto";
+        menu_display.style.zIndex = "200";
+        
+    }else{
+        menu_bar.style.width="0vw";
+        menu_bar.style.height="0vh";
+        checker -=1;
+        
+        logo_container.style.background="#f05454";  
+        menu_container.style.transform = "translateY(20px)"; 
+        menu_container.style.opacity = "0"; 
+        menu_container2.style.transform = "translateY(20px)"; 
+        menu_container2.style.opacity = "0"; 
+        menu_container3.style.transform = "translateY(20px)"; 
+        menu_container3.style.opacity = "0"; 
+        menu_container4.style.transform = "translateY(20px)"; 
+        menu_container4.style.opacity = "0"; 
+        menu_container5.style.transform = "translateY(20px)"; 
+        menu_container5.style.opacity = "0"; 
+        menu_container.style.cursor ="default";
+        menu_container.style.pointerEvents ="none";
+        menu_container2.style.cursor ="default";
+        menu_container2.style.pointerEvents ="none";
+        menu_container3.style.cursor ="default";
+        menu_container3.style.pointerEvents ="none";
+        menu_container4.style.cursor ="default";
+        menu_container4.style.pointerEvents ="none";
+        menu_container5.style.cursor ="default";
+        menu_container5.style.pointerEvents ="none";
+        menu_display.style.zIndex = "0";
+    }
+   
+})
+
+
+
+
+
+
+
+
